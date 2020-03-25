cea969ed18e7627b0525516d5f0e16599f5c1374
Sukhjit Singh <sukhjit2008@gmil.com>
2020-03-25 14:15:43 -0600

    css completed

--- src/totalAmount.js
+++ src/totalAmount.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const totalAmount = () => {
-  return <h3 className="heading-tertiary">Total Amount:</h3>;
+  return <h3 className="heading-tertiary">Total Amount: ${4000}</h3>;
 };
 
 export default totalAmount;
